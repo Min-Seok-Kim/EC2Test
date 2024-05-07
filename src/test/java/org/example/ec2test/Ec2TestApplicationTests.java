a692e686948ea3e78d6b9f1120abc66d0b9fd0fc
Min-Seok-Kim <70834609+Min-Seok-Kim@users.noreply.github.com>
2024-05-07 18:23:09 +0900

    first commit

---
+++ src/test/java/org/example/ec2test/Ec2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ec2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
